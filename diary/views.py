128a540180d70a6c36fe951f7bef516fd17e3104
furukawa2525 <ksw2070339@stu.o-hara.ac.jp>
2021-09-07 12:31:06 +0900

    0907s(css未反映)

---
+++ diary/views.py
@@ -0,0 +1,10 @@
+from django.views import generic
+from .forms import InquiryForm
+
+# Create your views here.
+class IndexView(generic.TemplateView):
+    template_name="index.html"
+
+class InquiryView(generic.FormView):
+    template_name="diary/templates/inquiry.html"
+    form_class=InquiryForm
