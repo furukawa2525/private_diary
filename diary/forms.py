4d82bbd0f4103919dab48c57d2bea108870d6a88
furukawa2525 <ksw2070339@stu.o-hara.ac.jp>
2021-09-14 18:38:04 +0000

    p280完成

--- diary/forms.py
+++ diary/forms.py
@@ -1,21 +1,26 @@
+from diary.models import Diary
 from django import forms
 from django.core.mail import EmailMessage
+from .models import Diary
 
 class InquiryForm(forms.Form):
-    name = forms.CharField(label='お名前', max_length=30)
+    name = forms.CharField(label='お名前',max_length=30)
     email = forms.EmailField(label='メールアドレス')
-    title = forms.CharField(label='タイトル', max_length=30)
-    message = forms.CharField(label='メッセージ', widget=forms.Textarea)
+    title = forms.CharField(label='タイトル',max_length=30)
+    message = forms.CharField(label='メッセージ',widget=forms.Textarea)
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    def __init__(self,*args,**kwargs):
+        super().__init__(*args,**kwargs)
 
         self.fields['name'].widget.attrs['class'] = 'form-control col-9'
         self.fields['name'].widget.attrs['placeholder'] = 'お名前をここに入力してください'
+
         self.fields['email'].widget.attrs['class'] = 'form-control col-11'
         self.fields['email'].widget.attrs['placeholder'] = 'メールアドレスをここに入力してください'
+
         self.fields['title'].widget.attrs['class'] = 'form-control col-11'
         self.fields['title'].widget.attrs['placeholder'] = 'タイトルをここに入力してください'
+
         self.fields['message'].widget.attrs['class'] = 'form-control col-12'
         self.fields['message'].widget.attrs['placeholder'] = 'メッセージをここに入力してください'
 
@@ -26,14 +31,20 @@ class InquiryForm(forms.Form):
         message = self.cleaned_data['message']
 
         subject = 'お問い合わせ {}'.format(title)
-        message = '送信者名: {0}\nメールアドレス: {1}\nメッセージ:\n{2}'.format(name, email, message)
-        from_email = 'admin@example.com'
-        to_list = [
-            'test@example.com'
-        ]
-        cc_list = [
-            email
-        ]
+        message = '送信者名 : {0}\nメールアドレス:{1}\nメッセージ:\n{2}'.format(name,email,message)
+        from_email = 'admin@examle.com'
+        to_list = ['text@example.com']
+        cc_list = [email]
 
         message = EmailMessage(subject=subject, body=message, from_email=from_email, to=to_list, cc=cc_list)
         message.send()
+
+class DiaryCreateForm(forms.ModelForm):
+    class Meta:
+        model = Diary
+        fields = ('title','content','photo1','photo2','photo3',)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs['class'] = 'form-control'
